171a3c2c7ca576bbf5c7727e52f82c296e31c535
axilirate <56825186+axilirate@users.noreply.github.com>
2021-06-03 18:26:32 +0300

    initial

---
+++ src/main/java/com/elunar/plugin/deathghost/DeathGhost.java
@@ -0,0 +1,17 @@
+package com.elunar.plugin.deathghost;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DeathGhost extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
