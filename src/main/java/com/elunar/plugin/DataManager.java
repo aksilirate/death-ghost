930cff4d0c72f65c2015794fb131bc29ab75fb96
axilirate <56825186+axilirate@users.noreply.github.com>
2021-06-08 20:28:16 +0300

    update

--- src/main/java/com/elunar/plugin/DataManager.java
+++ src/main/java/com/elunar/plugin/DataManager.java
@@ -1,5 +1,6 @@
 package com.elunar.plugin;
 
+import org.bukkit.Location;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.inventory.ItemStack;
 
@@ -9,8 +10,9 @@ import java.io.IOException;
 
 public class DataManager {
 
-    private DeathGhost deathGhost;
-    public DataManager(DeathGhost deathGhostClass){
+    public DeathGhost deathGhost;
+
+    public DataManager(DeathGhost deathGhostClass) {
         this.deathGhost = deathGhostClass;
     }
 
@@ -22,6 +24,62 @@ public class DataManager {
 
         saveYamlFile(file, yaml_file);
     }
+
+
+    public void setYamlPlayerGhostMode(String playerName, boolean mode) {
+        File file = new File(deathGhost.getDataFolder(), playerName + ".yml");
+        YamlConfiguration yaml_file = YamlConfiguration.loadConfiguration(file);
+
+        yaml_file.set("ghost_mode", mode);
+
+        saveYamlFile(file, yaml_file);
+    }
+
+
+    @SuppressWarnings({"ConstantConditions", "unused"})
+    public boolean getYamlPlayerGhostMode(String playerName) {
+        File file = new File(deathGhost.getDataFolder(), playerName + ".yml");
+        YamlConfiguration yaml_file = YamlConfiguration.loadConfiguration(file);
+
+        if (!yaml_file.contains("ghost_mode")) {
+            setYamlPlayerGhostMode(playerName, false);
+            return false;
+        } else {
+            return (boolean) yaml_file.get("ghost_mode");
+
+        }
+
+    }
+
+
+    public void setYamlPlayerDeathLocation(String playerName, Location location){
+        File file = new File(deathGhost.getDataFolder(), playerName + ".yml");
+        YamlConfiguration yaml_file = YamlConfiguration.loadConfiguration(file);
+
+        yaml_file.set("death_location", location);
+
+        saveYamlFile(file, yaml_file);
+    }
+
+
+    @SuppressWarnings("unused")
+    public Location getYamlPlayerDeathLocation(String playerName) {
+        File file = new File(deathGhost.getDataFolder(), playerName + ".yml");
+        YamlConfiguration yaml_file = YamlConfiguration.loadConfiguration(file);
+
+        if (!yaml_file.contains("death_location")) {
+            Location defaultLocation = new Location(deathGhost.getServer().getWorld("world"), 0.0, 0.0, 0.0);
+            setYamlPlayerDeathLocation(playerName, defaultLocation);
+            return defaultLocation;
+        } else {
+            return (Location) yaml_file.get("death_location");
+
+        }
+
+    }
+
+
+
 
 
     public void saveYamlFile(File file, YamlConfiguration yaml_file) {
--- src/main/java/com/elunar/plugin/EventListener.java
+++ src/main/java/com/elunar/plugin/EventListener.java
@@ -1,10 +1,13 @@
 package com.elunar.plugin;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
+
+import java.util.Locale;
 
 
 public class EventListener implements Listener {
@@ -22,11 +25,24 @@ public class EventListener implements Listener {
         Player player = event.getEntity();
 
         dataManager.setYamlPlayerInventory(player.getName(), player.getInventory().getContents());
+        dataManager.setYamlPlayerGhostMode(player.getName(), true);
+        dataManager.setYamlPlayerDeathLocation(player.getName(), player.getLocation());
+
+
+
+        player.getInventory().clear();
+        player.setInvulnerable(true);
+        player.setInvisible(true);
+        player.setFlying(true);
+
 
     }
 
     @EventHandler
     public void onPlayerRespawn(PlayerRespawnEvent event) {
+        Player player = event.getPlayer();
+        Location location = dataManager.getYamlPlayerDeathLocation(player.getName());
+        player.teleport(location);
 
     }
 
