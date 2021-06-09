f55c1ad69758341d3970467f484a4dc7276f2072
axilirate <56825186+axilirate@users.noreply.github.com>
2021-06-09 21:47:59 +0300

    update

--- src/main/java/com/elunar/plugin/DeathGhost.java
+++ src/main/java/com/elunar/plugin/DeathGhost.java
@@ -1,12 +1,16 @@
 package com.elunar.plugin;
 
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
 public class DeathGhost extends JavaPlugin {
 
+    private static Economy eco = null;
     public EventListener eventListener;
     public DataManager dataManager;
+
 
     @Override
     public void onEnable() {
@@ -15,12 +19,33 @@ public class DeathGhost extends JavaPlugin {
         dataManager = new DataManager(this);
         eventListener = new EventListener(this);
 
+
+        if (!setupEconomy() ) {
+            System.out.println("Disabled due to no Vault dependency found!");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+
         if (!getDataFolder().exists()) {
             if (getDataFolder().mkdirs()) {
                 getLogger().info("data dir was created.");
             }
-
         }
+
+
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        eco = rsp.getProvider();
+        return eco != null;
     }
 
 
--- src/main/java/com/elunar/plugin/EventListener.java
+++ src/main/java/com/elunar/plugin/EventListener.java
@@ -20,7 +20,7 @@ public class EventListener implements Listener {
     public RandomRespawn randomRespawn = new RandomRespawn();
     public ResetLocation resetLocation = new ResetLocation();
 
-    public EventListener(DeathGhost deathGhostClass){
+    public EventListener(DeathGhost deathGhostClass) {
         this.dataManager = new DataManager(deathGhostClass);
     }
 
@@ -33,7 +33,6 @@ public class EventListener implements Listener {
         dataManager.setYamlPlayerInventory(player.getName(), player.getInventory().getContents());
         dataManager.setYamlPlayerGhostMode(player.getName(), true);
         dataManager.setYamlPlayerDeathLocation(player.getName(), player.getLocation());
-
 
 
         player.getInventory().clear();
@@ -50,9 +49,9 @@ public class EventListener implements Listener {
         Location location = dataManager.getYamlPlayerDeathLocation(player.getName());
         event.getPlayer().getInventory().setHeldItemSlot(0);
 
-        event.getPlayer().getInventory().setItem(0,respawnHere.getItem());
-        event.getPlayer().getInventory().setItem(1,randomRespawn.getItem());
-        event.getPlayer().getInventory().setItem(8,resetLocation.getItem());
+        event.getPlayer().getInventory().setItem(0, respawnHere.getItem());
+        event.getPlayer().getInventory().setItem(1, randomRespawn.getItem());
+        event.getPlayer().getInventory().setItem(8, resetLocation.getItem());
 
         event.setRespawnLocation(location);
 
@@ -63,16 +62,18 @@ public class EventListener implements Listener {
     public void onPlayerDropItem(PlayerDropItemEvent event) {
         if (event.getItemDrop().getItemStack().equals(respawnHere.getItem())) {
             event.setCancelled(true);
+        } else if (event.getItemDrop().getItemStack().equals(randomRespawn.getItem())) {
+            event.setCancelled(true);
+        } else if (event.getItemDrop().getItemStack().equals(resetLocation.getItem())) {
+            event.setCancelled(true);
         }
 
     }
 
 
-
-
     @EventHandler
-    public void onPlayerPickupItem(PlayerPickupItemEvent event){
-        if (dataManager.getYamlPlayerGhostMode(event.getPlayer().getName())){
+    public void onPlayerPickupItem(PlayerPickupItemEvent event) {
+        if (dataManager.getYamlPlayerGhostMode(event.getPlayer().getName())) {
             event.setCancelled(true);
         }
     }
